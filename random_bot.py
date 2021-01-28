b3cf73fe34843caa98a4185dc4438493f50a9ae7
Sam Kriegman <sam.kriegman@uvm.edu>
2021-01-28 15:03:22 -0500

    Update random_bot.py

--- random_bot.py
+++ random_bot.py
@@ -1,20 +1,12 @@
-'''
-Author: Sam Kriegman
-'''
-
 import numpy as np
 from vxa_generator import vxa_from_array
 from scipy.ndimage.measurements import label
 
-'''
-Inputs:
-    WORKSPACE_LENGTH: Dimensions of the bounding box for the bot (i.e. bot exists within a cube with length WORKSPACE_LENGTH)
-    MUSCLE_ONLY: Flag specifying material. If True, uses active voxels only. If False uses both passive and active voxels.
-'''
 
-WORKSPACE_LENGTH = 6
-MUSCLE_ONLY = False
+WORKSPACE_LENGTH = 6  # length of the bounding box for the bot
+MUSCLE_ONLY = False  # If True, uses active voxels only. If False uses both passive and active voxels.
 
+n_mats = 2 if MUSCLE_ONLY else 3
 
 def make_one_shape_only(output_state):
     """Find the largest continuous arrangement of True elements after applying boolean mask.
@@ -49,11 +41,12 @@ def make_one_shape_only(output_state):
             
     return labeled == largest_label
 
-random_noise = np.random.randint(2, size=(WORKSPACE_LENGTH,)*3)
-if not MUSCLE_ONLY:
-    random_noise2 = np.random.randint(2, size=random_noise[random_noise==1].shape)
-    random_noise[random_noise==1]+=random_noise2
+
+random_noise = np.random.randint(n_mats, size=(WORKSPACE_LENGTH,)*3)
 mask = make_one_shape_only(random_noise)
+
 body = np.zeros_like(random_noise)
 body[mask] = random_noise[mask]
+
 vxa_from_array(body, save_filename='random_bot.vxa')
+
